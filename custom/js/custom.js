f0bf741bb0df46a98443034bea3ee6d9f91b4636
Saroz <skt.brt@gmail.com>
2018-12-29 16:51:06 +0545

    s

---
+++ custom/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#next").click(function(){
+        $("#step-2").show();
+        $("#step-1").hide();
+    });
+});
+
+function openMenu(evt, menuName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(menuName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
